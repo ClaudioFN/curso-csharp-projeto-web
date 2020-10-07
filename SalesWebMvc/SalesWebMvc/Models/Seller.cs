ed32720380fbaaa52b537414c9457db1a9444afa
ClaudioFN <clawdio30@gmail.com>
2020-10-06 23:33:45 -0300

    A262 e A263 - Uso de Annotations para Validar e Formatar e Tratamentos de Valicao
    A262 e A263 - Aplicacao para uso de validador via annotations e tag span com JavaScript.

--- SalesWebMvc/SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/SalesWebMvc/Models/Seller.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} tem que ser entre {2} e {1}")]
         public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Entre com um e-mail valido.")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "{0} required")]
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} deve ter valor entre {1} até {2}")]
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalery { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
